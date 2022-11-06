5c092af53685de58b4a73c46e759338985ba9b9c
fayyozbek <pazilovfa@gmail.com>
2022-11-07 00:37:51 +0500

    fisrt and last commit

---
+++ HelloGoogle/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+class Program{
+
+		static void Main(string[] arg){
+		ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+                {
+                    builder.AddConsole();
+                });
+                ILogger logger = loggerFactory.CreateLogger<Program>();
+				IWebDriver driver= new FirefoxDriver();
+				driver.Url="https://www.google.ru/";
+				driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+				driver.	FindElement(By.Name("q")).SendKeys("Hello World"+Keys.Enter);
+				WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+				wait.Until(driver => driver.Title.Contains("Hello"));
+				logger.LogInformation(driver.Title);
+
+
+		}
+	}
+
+
+
